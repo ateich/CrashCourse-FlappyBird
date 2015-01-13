4cc3227aeb99755b7631f9e9b87d08cfaa8ce5c9
Andrew Teich <andrewteich@me.com>
2015-01-12 17:09:45 -0800

    Initial Commit

---
+++ CrashCourse-FlappyBird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrashCourse-FlappyBird
+//
+//  Created by HackReactor on 1/12/15.
+//  Copyright (c) 2015 HackReactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CrashCourse-FlappyBird/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CrashCourse-FlappyBird
+//
+//  Created by HackReactor on 1/12/15.
+//  Copyright (c) 2015 HackReactor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
